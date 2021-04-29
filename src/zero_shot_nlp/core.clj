ed7747e234e61f491e90573d86bdbd33f15a0b1a
Shane Mulligan <mullikine@gmail.com>
2021-04-30 00:16:02 +1200

    Fri 30 Apr 2021 00:16:02

--- src/zero_shot_nlp/core.clj
+++ src/zero_shot_nlp/core.clj
@@ -1,7 +1,17 @@
 (ns zero-shot-nlp.core
-  (:gen-class))
+  (:gen-class)
+  (:require [libpython-clj2.require :refer [require-python]]
+            [libpython-clj2.python :as py :refer [py. py.. py.-]]))
+
+(require-python '[transformers :bind-ns])
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "I classify stuff."
   [& args]
-  (println "Hello, World!"))
+  (def classifier (py. transformers "pipeline" "zero-shot-classification"))
+
+  (def text "French Toast with egg and bacon in the center with maple syrup on top. Sprinkle with powdered sugar if desired.")
+
+  (def labels ["breakfast" "lunch" "dinner"])
+
+  (classifier text labels))
